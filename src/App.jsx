d66c0fe2f2046acbb956c08cbe86cf0e2a27ebca
Will Haynes <wileland7@gmail.com>
2025-05-21 12:27:10 -0500

    Updated dependencies to include three.js etc, updated App.jsx for route plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GameRoute from './pages/GameRoute'; // Make sure this path is correct
+
 import Test from "./Test";
 import "./app.scss";
 import Contact from "./components/contact/Contact";
@@ -10,29 +13,40 @@ import Services from "./components/services/Services";
 
 const App = () => {
   return (
-    <div>
-      <Cursor />
-      <section id="Homepage">
-        <Navbar />
-        <Hero />
-      </section>
-      <section id="Services">
-        <Parallax type="services" />
-      </section>
-      <section>
-        <Services />
-      </section>
-      <section id="Portfolio">
-        <Parallax type="portfolio" />
-      </section>
-      <Portfolio />
-      <section id="Contact">
-        <Contact />
-      </section>
-      {/* Framer Motion Crash Course */}
-      {/* <Test/>
-    <Test/> */}
-    </div>
+    <Router>
+      <Cursor /> {/* Cursor can be here if it's global and doesn't interfere with routing */}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <> {/* Using a fragment to group existing sections for the root path */}
+              <section id="Homepage">
+                <Navbar />
+                <Hero />
+              </section>
+              <section id="Services">
+                <Parallax type="services" />
+              </section>
+              <section>
+                <Services />
+              </section>
+              <section id="Portfolio">
+                <Parallax type="portfolio" />
+              </section>
+              <Portfolio /> {/* Assuming this is a component and not a section tag based on naming */}
+              <section id="Contact">
+                <Contact />
+              </section>
+              {/* Framer Motion Crash Course */}
+              {/* <Test/>
+              <Test/> */}
+            </>
+          }
+        />
+        <Route path="/game" element={<GameRoute />} />
+        {/* You can add other routes here later */}
+      </Routes>
+    </Router>
   );
 };
 
